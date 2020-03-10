38274fa2565837bb508e82c1d1a4c03273e6b158
GonASSalo Batalhao Alves <up201806451@feup-b201d12.fe.up.pt>
2020-03-10 14:31:05 +0000

    Finished ex8

--- TP_03/p8.c
+++ TP_03/p8.c
@@ -3,8 +3,10 @@
 #include <sys/types.h> 
 #include <unistd.h> 
 #include <stdlib.h> 
+#include <string.h>
 int main(int argc, char *argv[], char *envp[]) {    
-    pid_t pid;    
+    pid_t pid;
+    char *com[] = {"ls", "-laR", NULL};
     if (argc != 2) {      
         printf("usage: %s dirname\n",argv[0]);      
         exit(1);    
@@ -13,7 +15,11 @@ int main(int argc, char *argv[], char *envp[]) {
     if (pid > 0)      
         printf("My child is going to execute command \"ls -laR %s\"\n", argv[1]);    
     else if (pid == 0){      
-        execlp("ls","ls","-laR",argv[1],NULL);      
+        //execlp("ls","ls","-laR",argv[1],NULL);      
+        //execl("/bin/ls","ls","-laR",argv[1],NULL);
+        //execvp("ls",com);    
+        //execv("/bin/ls",com);
+        execve("/bin/ls",com,envp);
         printf("Command not executed !\n");      
         exit(1);    
     }    
