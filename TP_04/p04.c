2dae1f3b6c35007d82f2cb9545a0c8a04580be33
g-batalhao-a <al.darkseid.2010@gmail.com>
2020-03-17 14:35:13 +0000

    Ex4 working properly

--- TP_04/p04.c
+++ TP_04/p04.c
@@ -15,15 +15,15 @@ int main(void) {
             sleep(i*7); // child working ...       
             printf("CHILD no. %d (PID=%d) exiting ... \n",i,getpid());        
             exit(0);     
-        }   
+        }
     }   
-    for (i=1 ;i<=4; i++ ) {      
+    for (i=1 ;i<=4; i++) {      
         printf("PARENT: working hard (task no. %d) ...\n",i);     
         n=20; while((n=sleep(n))!=0);     
         printf("PARENT: end of task no. %d\n",i);     
         printf("PARENT: waiting for child no. %d ...\n",i);     
         //pid=wait(&status);   alinea a
-        pid=waitpid(-1,&status,WNOHANG);   
+        pid=waitpid(0,&status,WNOHANG);   
         if (pid != -1)       
             printf("PARENT: child with PID=%d terminated with exit code %d\n",pid,WEXITSTATUS(status));   
     }   
